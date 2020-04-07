ccf19be1a188f7d8b8225e2fa9e0ecebbe3bf4d7
Xavier Perseguers <xavier@causal.ch>
2020-04-07 16:57:10 +0200

    [TASK] Completely rework the fields of staffdirectory fe_users records

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -1,6 +1,31 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+// Create a dedicated type of records to ease the management of those fe_users records in the context of this staffs
+
+$GLOBALS['TCA']['fe_users']['columns']['tx_extbase_type']['config']['items'][] = [
+    'Staffdirectory Member',
+    'tx_staffdirectory'
+];
+
+$GLOBALS['TCA']['fe_users']['types']['tx_staffdirectory'] = [
+    'showitem' => '
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:fe_users.tabs.personelData,
+                    --palette--;;sd_name, --palette--;;sd_contact, --palette--;;sd_address, image,                
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                    disable,--palette--;;timeRestriction,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    username,password,usergroup,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+                    tx_extbase_type
+            ',
+];
+$GLOBALS['TCA']['fe_users']['palettes'] += [
+    'sd_name' => ['showitem' => 'title,first_name,last_name'],
+    'sd_contact' => ['showitem' => 'telephone,tx_staffdirectory_mobilephone,--linebreak--,email,tx_staffdirectory_email2'],
+    'sd_address' => ['showitem' => 'address,--linebreak--,zip,--linebreak--,city,--linebreak--,country'],
+];
+
 $tempColumns = [
     'tx_staffdirectory_mobilephone' => [
         'exclude' => 0,
@@ -28,7 +53,7 @@ $tempColumns = [
     ],
     'tx_staffdirectory_email2' => [
         'exclude' => 1,
-        'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.email',
+        'label' => 'LLL:EXT:staffdirectory/Resources/Private/Language/locallang_db.xlf:fe_users.tx_staffdirectory_email2',
         'config' => [
             'type' => 'input',
             'size' => '20',
@@ -39,8 +64,8 @@ $tempColumns = [
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumns, 1);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_staffdirectory_gender', '', 'before:address');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_staffdirectory_mobilephone', '', 'after:telephone');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_staffdirectory_gender', '', 'before:title');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_staffdirectory_mobilephone', '0', 'after:telephone');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_staffdirectory_email2', '', 'after:email');
 
 $GLOBALS['TCA']['fe_users']['ctrl']['label'] = 'last_name';
