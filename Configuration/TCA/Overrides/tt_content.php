780c57f0d8b68006f67ddfe4c86e887f7335cd17
Xavier Perseguers <xavier@causal.ch>
2023-12-02 15:37:36 +0100

    [TASK] Drop everything that is not used anymore

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -55,19 +55,3 @@ $GLOBALS['TCA']['tt_content']['types']['staffdirectory_plugin']['columnsOverride
  */
 $GLOBALS['TCA']['tt_content']['types']['staffdirectory_plugin']['previewRenderer']
     = \Causal\Staffdirectory\Preview\PluginPreviewRenderer::class;
-
-// Register Frontend plugin
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'staffdirectory',
-    'Pi1',
-    'LLL:EXT:staffdirectory/Resources/Private/Language/locallang_db.xlf:tt_content.list_type_pi1'
-);
-
-$pluginSignature = 'staffdirectory_pi1';
-
-// Disable the display of layout, select_key and page fields
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-
-// Register the FlexForms
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('staffdirectory_pi1', 'FILE:EXT:staffdirectory/Configuration/FlexForms/Pi1.xml');
--- ext_localconf.php
+++ ext_localconf.php
@@ -25,23 +25,12 @@ defined('TYPO3') || die ();
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
     );
 
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        $_EXTKEY,
-        'Pi1',
-        // cacheable actions
-        [
-            \Causal\Staffdirectory\Controller\StaffController::class => 'dispatch, list, staff, person, persons, directory',
-        ],
-        // non-cacheable actions
-        []
-    );
-
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
     @import \'EXT:staffdirectory/Configuration/TSconfig/ContentElementWizard.tsconfig\'
     ');
 
-    $versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
-    if ($versionInformation->getMajorVersion() >= 12) {
+    $typo3Version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class)->getMajorVersion();
+    if ($typo3Version >= 12) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Form\Container\InlineRecordContainer::class] = [
             'className' => \Causal\Staffdirectory\Xclass\V12\Backend\Form\Container\InlineRecordContainer::class,
         ];
@@ -50,11 +39,4 @@ defined('TYPO3') || die ();
             'className' => \Causal\Staffdirectory\Xclass\V11\Backend\Form\Container\InlineRecordContainer::class,
         ];
     }
-
-    /* ===========================================================================
-        Web > Page hook
-    =========================================================================== */
-    $pluginSignature = 'staffdirectory_pi1';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info'][$pluginSignature][$_EXTKEY] =
-        \Causal\Staffdirectory\Hooks\PageLayoutView::class . '->getExtensionSummary';
 })('staffdirectory');
