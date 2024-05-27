9f899151a405f76f560f2a7f01f4ed6f60947f1e
Xavier Perseguers <xavier@causal.ch>
2024-05-27 12:48:24 +0200

    [TASK] Raise PHP dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'xavier@causal.ch',
     'author_company' => 'Causal Sàrl',
     'state' => 'stable',
-    'version' => '2.0.0',
+    'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.2.99',
+            'php' => '8.1.0-8.3.99',
             'typo3' => '11.5.0-12.4.99',
             'static_info_tables' => '11.5.0-12.4.99'
         ],
