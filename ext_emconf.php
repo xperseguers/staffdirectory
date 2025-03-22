e1ecfc415d836a54f3a2ebf3c09e63809ee94f27
Xavier Perseguers <xavier@causal.ch>
2025-03-22 18:06:50 +0100

    [TASK] Adapt dependencies and mark PHP 8.4 as supported

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.3.99',
-            'typo3' => '11.5.0-12.4.99',
-            'static_info_tables' => '11.5.0-12.4.99'
+            'php' => '8.1.0-8.4.99',
+            'typo3' => '11.5.0-13.4.99',
+            'static_info_tables' => '11.5.0-13.4.99'
         ],
         'conflicts' => [
         ],
